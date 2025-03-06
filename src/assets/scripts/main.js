c9f91c2aeafcc0e6ef591444055a0135698cc335
Sergio Cozar <sergiocozar.r@gmail.com>
2025-03-06 18:49:30 +0100

    feat: web created

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,17 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+import "@fortawesome/fontawesome-free/js/all";
+
+/**
+ * Write any other JavaScript below
+ */
+
++(function () {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+})();
